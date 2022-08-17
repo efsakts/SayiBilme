8f81588d693489e3524edd133a7a16f5fdd42f9c
efsakts <efsaaktas@outlook.com>
2022-08-17 09:58:34 +0300

    Add project files.

---
+++ SayiBilme/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            Random ran = new Random();
+            int holdNumber = ran.Next(1000, 9999);
+            Console.WriteLine("guess a 4-digit number");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            while (guess != holdNumber)
+            {
+
+
+                counter++;
+
+
+                if (holdNumber < guess)
+                {
+                    Console.WriteLine("enter bigger number");
+                }
+                else if (holdNumber > guess)
+                {
+                    Console.WriteLine("enter smaller number");
+                }
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Well done!.");
+            Console.WriteLine("You know on the {0}. guess", counter);
+        }
+    }
+}
